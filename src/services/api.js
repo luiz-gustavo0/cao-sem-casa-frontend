566c7d710b66cf65e84a216386e591b70b0a7a57
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-06-14 23:25:48 -0300

    adciona url api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:4000'
-  baseURL: 'https://cao-sem-casa-backend.herokuapp.com'
+  // eslint-disable-next-line no-undef
+  baseURL: process.env.REACT_APP_LINK_API
 })
 
 export default api
