28be9d8383624b7d1307df0addac81a9c606fbe0
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-06-14 23:38:03 -0300

    corrige baseUrl axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-
-  baseURL: 'http://localhost:4000'
-
   // baseURL: 'http://localhost:4000'
   // eslint-disable-next-line no-undef
   baseURL: process.env.REACT_APP_LINK_API
-
 })
 
 export default api
