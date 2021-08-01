72ce1774fb6f40d334b8f73705bf73cf7f080cd3
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-07-31 21:13:27 -0300

    adiciona pagina de recuperação de senha

---
+++ src/pages/ResetPassword/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <input type="email" name="email" id="email" />
+    </div>
+  )
+}
+
+export default ResetPassword
